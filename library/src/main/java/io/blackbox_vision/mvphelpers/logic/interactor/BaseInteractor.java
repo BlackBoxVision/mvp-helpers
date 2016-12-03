d5ce5b9c391653c0642e0e3293f3a5fc9d9935ea
jonatansalas <jonatansalas@live.com.ar>
2016-12-03 17:46:55 -0300

    #3 Update BaseInteractor class in order to recycle and reuse ThreadPool

--- library/src/main/java/io/blackbox_vision/mvphelpers/logic/interactor/BaseInteractor.java
+++ library/src/main/java/io/blackbox_vision/mvphelpers/logic/interactor/BaseInteractor.java
@@ -4,9 +4,9 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 
-
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
@@ -15,25 +15,13 @@ import io.blackbox_vision.mvphelpers.utils.Preconditions;
 
 
 public class BaseInteractor {
-    private ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(5);
-    private ScheduledFuture<?> scheduledFuture;
+    private static final int NUMBER_OF_THREADS = 5;
 
-    private Executor executor = Executors.newFixedThreadPool(5);
+    private ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(NUMBER_OF_THREADS);
+    private ExecutorService executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
     private Handler handler = new Handler(Looper.getMainLooper());
 
-    protected void runOnBackground(@NonNull Runnable runnable, @NonNull Long time, @NonNull TimeUnit unit) {
-        Preconditions.checkNotNull(runnable, "runnable shouldn't be null");
-        Preconditions.checkNotNull(time, "time shouldn't be null");
-        Preconditions.checkNotNull(unit, "unit shouldn't be null");
-
-        scheduledFuture = scheduledExecutor.schedule(runnable, time, unit);
-    }
-
-    protected void runOnBackground(@NonNull Runnable runnable) {
-        Preconditions.checkNotNull(runnable, "runnable shouldn't be null");
-
-        executor.execute(runnable);
-    }
+    private ScheduledFuture<?> scheduledFuture;
 
     protected void runOnUiThread(@NonNull Runnable runnable) {
         Preconditions.checkNotNull(runnable, "runnable shouldn't be null");
@@ -41,7 +29,29 @@ public class BaseInteractor {
         handler.post(runnable);
     }
 
-    protected void cancelTask() {
+    protected void runOnBackground(@NonNull Runnable runnable) {
+        Preconditions.checkNotNull(runnable, "runnable shouldn't be null");
+
+        if (executor.isTerminated()) {
+            executor = Executors.newFixedThreadPool(5);
+        }
+
+        final Future<?> task = executor.submit(runnable);
+
+        if (task.isDone()) {
+            executor.shutdown();
+        }
+    }
+
+    protected void runScheduledTaskOnBackground(@NonNull Runnable runnable, @NonNull Long time, @NonNull TimeUnit unit) {
+        Preconditions.checkNotNull(runnable, "runnable shouldn't be null");
+        Preconditions.checkNotNull(time, "time shouldn't be null");
+        Preconditions.checkNotNull(unit, "unit shouldn't be null");
+
+        scheduledFuture = scheduledExecutor.schedule(runnable, time, unit);
+    }
+
+    protected void cancelScheduledTask() {
         if (null != scheduledFuture) {
             scheduledFuture.cancel(true);
         }
